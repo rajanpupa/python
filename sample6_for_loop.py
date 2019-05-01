fd3ec82a44755a84ba8d086ceaa454600a35c1b9
Rajan <rajanpupa@gmail.com>
2019-04-30 21:42:04 -0400

    for loop

---
+++ sample6_for_loop.py
@@ -0,0 +1,11 @@
+# range is from 0 to n-1
+for i in range(5):
+    print(i)
+
+# another range example (11,12,13,14)
+for j in range(11, 15):
+    print(j)
+
+fruits = ["apple", "orange", "banana"]
+for x in fruits:
+    print(x)
