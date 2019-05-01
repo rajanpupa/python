154b5b758d3adb0992bc72b36e06fbb4d8b6902a
Rajan <rajanpupa@gmail.com>
2019-04-30 21:44:05 -0400

    more for loop examples

--- sample6_for_loop.py
+++ sample6_for_loop.py
@@ -6,6 +6,13 @@ for i in range(5):
 for j in range(11, 15):
     print(j)
 
+# auto iterator for loop example
 fruits = ["apple", "orange", "banana"]
 for x in fruits:
     print(x)
+
+# increment the range by 3, and else, example
+for x in range(2, 30, 3):
+  print(x)
+else:
+  print("Finally finished!")
