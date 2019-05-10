5269095c063d9cb9ba1c96ea9b0bb845f3fb398e
Rajan <rajanpupa@gmail.com>
2019-05-09 20:56:20 -0400

    renamed

---
+++ sample2_if_elif.py
@@ -0,0 +1,15 @@
+x=5
+if  x<10:
+    print('Smaller')
+elif x < 20 :
+    print('Bigger')
+else:
+    print('Very big')
+print ('Finis')
+
+smallest = 0
+value = 'hello'
+if smallest is None : 
+    smallest = value
+
+print(smallest)
